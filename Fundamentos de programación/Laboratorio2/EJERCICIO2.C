7b9737f6f0349aec18723b0eeb8a9054f5eaf1df
daniel-gcp <74461547+daniel-gcp@users.noreply.github.com>
2020-12-15 05:19:19 -0500

    Add files via upload

---
+++ Fundamentos de programación/Laboratorio2/EJERCICIO2.C
@@ -0,0 +1,43 @@
+/* Diseñar una función que informe si una cadena es un palíndromo (una
+cadena es un palíndromo si se lee igual de izquierda a derecha que de
+derecha a izquierda). Ejemplo: radar*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#define MAX 25
+
+int main()
+{
+    int p_i, p_f;
+    char Palabra[MAX], caracter;
+    int i;
+    
+    printf("\nIntroduzca la palabra: ");
+    scanf("%c", &caracter);
+    i=0;
+    while(i<MAX && caracter !='.')
+    {
+    Palabra [i]=caracter;
+    i++;
+    scanf("%c", &caracter);
+}
+   
+   p_i=0;
+   p_f=i-1;
+   
+   while(p_i<p_f && Palabra[p_i]==Palabra[p_f])
+   {
+                 p_i++;
+                 p_f--;
+}
+
+if(p_i>=p_f) printf("\nLa palabra es palindroma!\n");
+else printf("\nLa palabra no es palindroma!\n");
+              
+   
+    
+    printf("\n\n");
+    return 0;
+}
+
+
